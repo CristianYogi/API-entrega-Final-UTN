76c5e01ffd670e1c92dacb062290200063197592
CristianYogi <yogi@live.com.ar>
2022-06-20 19:21:15 -0300

    a

--- utlis/handleStorage.js
+++ utlis/handleStorage.js
@@ -1,19 +1,4 @@
-const multer = require("multer");
-
-const storage = multer.diskStorage({
-    destination:(req, file, callback) => {
-        const pathStorage = `${__dirname}/../img_storage`
-        console.log('FILE',file)
-        callback(null, pathStorage)
-    },
-    filename:(req, file, callback) => {
-        const ext = file.originalname.split(".").pop()
-        const filename = `img-${Date.now()}.${ext}`
-        console.log(filename)
-        callback(null, filename)
-    }
-})
-
+const storage = new GridFsStorage({ url: process.env.MONGODB_URI, file: (req, file) => { return new Promise((resolve, reject) => { crypto.randomBytes(16, (err, buf) => { if (err) { return reject(err); } const filename = file.originalname; const fileInfo = { filename: filename, bucketName: "uploads" }; resolve(fileInfo); }); }); } });
 const fileUpload = multer({storage})
 
 module.exports = fileUpload
