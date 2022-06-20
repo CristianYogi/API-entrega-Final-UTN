f0bfcb2f66998ad6caaf6b9b35581d09b6edb1b4
CristianYogi <yogi@live.com.ar>
2022-06-19 21:23:39 -0300

    a

--- user/usersModel.js
+++ user/usersModel.js
@@ -4,10 +4,11 @@ const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
     nombre: {type: String, required: true},
+    apellido: {type: String, required: true},
     userName: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    telefono: {type: Number}
+    phoneNumber: {type: Number}
 },
     {timestamps: true} //crea campos de el momento en que fue creado o actualizado
 )
